4a9e1cdefacd4db860db15fd0d761d105ed47622
Gagaro <gagaro42@gmail.com>
2025-01-10 10:50:45 +0100

    Fix OS-1345: ajout de l'onglet Historique

--- templatetags/parcours_doctoral.py
+++ templatetags/parcours_doctoral.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2024 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2025 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -37,7 +37,6 @@ from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import pgettext
 from osis_document.api.utils import get_remote_metadata, get_remote_token
-from reference.models.language import Language
 
 from parcours_doctoral.auth.constants import READ_ACTIONS_BY_TAB, UPDATE_ACTIONS_BY_TAB
 from parcours_doctoral.constants import CAMPUSES_UUIDS
@@ -49,6 +48,7 @@ from parcours_doctoral.ddd.formation.domain.model.enums import (
 )
 from parcours_doctoral.ddd.repository.i_parcours_doctoral import formater_reference
 from parcours_doctoral.models import ParcoursDoctoral
+from reference.models.language import Language
 
 register = template.Library()
 
@@ -79,10 +79,6 @@ TAB_TREE = {
     # Tab('comments', pgettext('tab', 'Comments'), 'comments'): [
     #     Tab('comments', pgettext('tab', 'Comments'), 'comments')
     # ],
-    # Tab('history', pgettext('tab', 'History'), 'history'): [
-    #     Tab('history-all', _('All history')),
-    #     Tab('history', _('Status changes')),
-    # ],
     # Tab('person', _('Personal data'), 'user'): [
     #     Tab('person', _('Identification'), 'user'),
     #     Tab('coordonnees', _('Contact details'), 'user'),
@@ -111,6 +107,10 @@ TAB_TREE = {
     Tab('defense', pgettext('doctorate tab', 'Defense'), 'person-chalkboard'): [
         Tab('jury-preparation', pgettext('admission tab', 'Defense method')),
         Tab('jury', _('Jury composition')),
+    ],
+    Tab('history', pgettext('tab', 'History'), 'history'): [
+        Tab('history-all', _('All history')),
+        Tab('history', _('Status changes')),
     ],
     Tab('management', pgettext('tab', 'Management'), 'gear'): [
         Tab('send-mail', _('Send a mail')),
---
+++ views/details/history.py
@@ -0,0 +1,63 @@
+# ##############################################################################
+#
+#  OSIS stands for Open Student Information System. It's an application
+#  designed to manage the core business of higher education institutions,
+#  such as universities, faculties, institutes and professional schools.
+#  The core business involves the administration of students, teachers,
+#  courses, programs and so on.
+#
+#  Copyright (C) 2015-2025 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of this license - GNU General Public License - is available
+#  at the root of the source code of this program.  If not,
+#  see http://www.gnu.org/licenses/.
+#
+# ##############################################################################
+
+from django.views.generic import TemplateView
+from osis_history.contrib.mixins import HistoryEntryListAPIMixin
+from rules.contrib.views import LoginRequiredMixin
+
+from osis_role.contrib.views import APIPermissionRequiredMixin
+from parcours_doctoral.utils.cache import get_cached_parcours_doctoral_perm_obj
+from parcours_doctoral.views.mixins import ParcoursDoctoralViewMixin
+
+__all__ = [
+    "HistoryAPIView",
+    "HistoryView",
+    "HistoryAllView",
+]
+__namespace__ = False
+
+
+class HistoryAPIView(LoginRequiredMixin, APIPermissionRequiredMixin, HistoryEntryListAPIMixin):
+    urlpatterns = 'history-api'
+    permission_mapping = {
+        'GET': 'parcours_doctoral.view_historyentry',
+    }
+
+    def get_permission_object(self):
+        get_cached_parcours_doctoral_perm_obj(self.kwargs['uuid'])
+
+
+class HistoryView(ParcoursDoctoralViewMixin, TemplateView):
+    urlpatterns = 'history'
+    template_name = 'parcours_doctoral/details/history.html'
+    permission_required = 'parcours_doctoral.view_historyentry'
+    extra_context = {'tag': 'status-changed'}
+
+
+class HistoryAllView(ParcoursDoctoralViewMixin, TemplateView):
+    urlpatterns = 'history-all'
+    template_name = 'parcours_doctoral/details/history.html'
+    permission_required = 'parcours_doctoral.view_historyentry'
