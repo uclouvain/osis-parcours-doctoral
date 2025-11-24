9f7233214ce9805a7a66195c3c3544307e301ef0
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2025-11-24 17:48:05 +0100

    [OS-354] Thesis distribution authorization > allow the reference promoter to accept the thesis

--- tests/api/views/authorisation_distribution/test_manuscript_validation.py
+++ tests/api/views/authorisation_distribution/test_manuscript_validation.py
@@ -26,6 +26,7 @@
 from email import message_from_string
 from email.utils import getaddresses
 
+import freezegun
 from django.db.models import QuerySet
 from django.shortcuts import resolve_url
 from osis_notification.models import EmailNotification
@@ -40,11 +41,12 @@ from parcours_doctoral.ddd.autorisation_diffusion_these.domain.model.enums impor
     RoleActeur,
 )
 from parcours_doctoral.ddd.autorisation_diffusion_these.domain.validator.exceptions import (
+    GestionnaireADRENonTrouveException,
     MotifRefusNonSpecifieException,
     SignataireNonInviteException,
 )
 from parcours_doctoral.ddd.domain.model.enums import ChoixStatutParcoursDoctoral
-from parcours_doctoral.ddd.jury.domain.model.enums import FormuleDefense
+from parcours_doctoral.ddd.jury.domain.model.enums import FormuleDefense, RoleJury
 from parcours_doctoral.models import ParcoursDoctoral
 from parcours_doctoral.models.thesis_distribution_authorization import (
     ThesisDistributionAuthorization,
@@ -54,6 +56,7 @@ from parcours_doctoral.tests.factories.authorization_distribution import (
     PromoterThesisDistributionAuthorizationActorFactory,
     ThesisDistributionAuthorizationFactory,
 )
+from parcours_doctoral.tests.factories.jury import JuryActorFactory
 from parcours_doctoral.tests.factories.parcours_doctoral import ParcoursDoctoralFactory
 from parcours_doctoral.tests.factories.roles import (
     ScebManagerRoleFactory,
@@ -70,12 +73,17 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         cls.sceb_manager = ScebManagerRoleFactory().person
         cls.language = LanguageFactory()
 
-        cls.data = {
+        cls.reject_data = {
             'motif_refus': 'Reason',
-            'commentaire_interne': 'Internal comment',
-            'commentaire_externe': 'External comment',
+            'commentaire_interne': 'Internal comment 0',
+            'commentaire_externe': 'External comment 0',
+        }
+        cls.accept_data = {
+            'commentaire_interne': 'Internal comment 1',
+            'commentaire_externe': 'External comment 1',
         }
 
+    @freezegun.freeze_time('2024-01-01')
     def setUp(self):
         self.doctorate: ParcoursDoctoral = ParcoursDoctoralFactory(
             student=self.doctorate_student,
@@ -83,16 +91,25 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
             defense_method=FormuleDefense.FORMULE_1.name,
         )
 
-        lead_promoter = self.doctorate.supervision_group.actors.filter(
-            parcoursdoctoralsupervisionactor__is_reference_promoter=True,
-        ).first()
-        self.promoter: ThesisDistributionAuthorizationActor = PromoterThesisDistributionAuthorizationActorFactory(
-            person=lead_promoter.person,
+        # Promoter
+        jury_promoter = self.doctorate.jury_group.actors.filter(juryactor__is_lead_promoter=True).first()
+        jury_promoter.person = (
+            self.doctorate.supervision_group.actors.filter(parcoursdoctoralsupervisionactor__is_reference_promoter=True)
+            .first()
+            .person
         )
-        self.invited_state = StateHistory.objects.create(
+        jury_promoter.save()
+        self.promoter: ThesisDistributionAuthorizationActor = PromoterThesisDistributionAuthorizationActorFactory(
+            person=jury_promoter.person,
+        )
+        self.promoter_invited_state = StateHistory.objects.create(
             actor=self.promoter,
             state=SignatureState.INVITED.name,
         )
+
+        # Adre
+        self.jury_adre = JuryActorFactory(process=jury_promoter.process, role=RoleJury.ADRE.name)
+
         self.thesis_distribution_authorization: ThesisDistributionAuthorization = (
             ThesisDistributionAuthorizationFactory(
                 parcours_doctoral=self.doctorate,
@@ -110,7 +127,6 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         methods_not_allowed = [
             'delete',
             'patch',
-            'post',
         ]
 
         for method in methods_not_allowed:
@@ -139,7 +155,7 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         self.doctorate.save()
 
         # > during the private defense
-        response = self.client.put(self.url, data=self.data)
+        response = self.client.put(self.url, data=self.reject_data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
         # Second defense method
@@ -149,7 +165,7 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         self.doctorate.save()
 
         # > during the admissibility
-        response = self.client.put(self.url, data=self.data)
+        response = self.client.put(self.url, data=self.reject_data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
         # > after the admissibility but the authorization is not submitted
@@ -159,13 +175,13 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         self.thesis_distribution_authorization.status = ChoixStatutAutorisationDiffusionThese.DIFFUSION_NON_SOUMISE.name
         self.thesis_distribution_authorization.save()
 
-        response = self.client.put(self.url, data=self.data)
+        response = self.client.put(self.url, data=self.reject_data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_refuse_the_thesis_by_the_lead_promoter_with_no_reason(self):
         self.client.force_authenticate(user=self.promoter.person.user)
 
-        data = self.data.copy()
+        data = self.reject_data.copy()
         data['motif_refus'] = ''
 
         response = self.client.put(self.url, data=data)
@@ -181,9 +197,9 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
     def test_refuse_the_thesis_by_the_lead_promoter_with_no_invited_promoter(self):
         self.client.force_authenticate(user=self.promoter.person.user)
 
-        self.invited_state.delete()
+        self.promoter_invited_state.delete()
 
-        response = self.client.put(self.url, data=self.data)
+        response = self.client.put(self.url, data=self.reject_data)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -196,7 +212,7 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
     def test_refuse_the_thesis_by_the_lead_promoter(self):
         self.client.force_authenticate(user=self.promoter.person.user)
 
-        response = self.client.put(self.url, data=self.data)
+        response = self.client.put(self.url, data=self.reject_data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -213,21 +229,27 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         group = self.thesis_distribution_authorization.signature_group
         self.assertIsNotNone(group)
 
-        actors: QuerySet[Actor] = group.actors.all()
-        self.assertEqual(len(actors), 1)
-
-        self.assertEqual(actors[0].person, self.promoter.person)
-        self.assertEqual(actors[0].thesisdistributionauthorizationactor.rejection_reason, self.data['motif_refus'])
-        self.assertEqual(
-            actors[0].thesisdistributionauthorizationactor.internal_comment, self.data['commentaire_interne']
+        promoters: QuerySet[Actor] = group.actors.filter(
+            thesisdistributionauthorizationactor__role=RoleActeur.PROMOTEUR.name,
         )
-        self.assertEqual(actors[0].thesisdistributionauthorizationactor.role, RoleActeur.PROMOTEUR.name)
-        self.assertEqual(actors[0].comment, self.data['commentaire_externe'])
+        self.assertEqual(len(promoters), 1)
 
-        states: QuerySet[StateHistory] = actors[0].states.all()
+        self.assertEqual(promoters[0].person, self.promoter.person)
+        self.assertEqual(
+            promoters[0].thesisdistributionauthorizationactor.rejection_reason,
+            self.reject_data['motif_refus'],
+        )
+        self.assertEqual(
+            promoters[0].thesisdistributionauthorizationactor.internal_comment,
+            self.reject_data['commentaire_interne'],
+        )
+        self.assertEqual(promoters[0].thesisdistributionauthorizationactor.role, RoleActeur.PROMOTEUR.name)
+        self.assertEqual(promoters[0].comment, self.reject_data['commentaire_externe'])
+
+        states: QuerySet[StateHistory] = promoters[0].states.all()
         self.assertEqual(len(states), 2)
 
-        self.assertEqual(states[0].state, SignatureState.DECLINED.name)
+        self.assertEqual(states[1].state, SignatureState.DECLINED.name)
 
         # Check that the notification has been sent
         self.assertEqual(EmailNotification.objects.count(), 1)
@@ -240,5 +262,105 @@ class ManuscriptValidationAPIViewTestCase(APITestCase):
         cc_email_addresses = [address for _, address in getaddresses(email_message.get_all('Cc', [('', '')]))]
         self.assertEqual(len(to_email_addresses), 1)
         self.assertEqual(to_email_addresses[0], self.doctorate.student.email)
+        self.assertEqual(len(cc_email_addresses), 1)
+        self.assertEqual(cc_email_addresses, [''])
+
+    def test_accept_the_thesis_by_the_lead_promoter_with_no_invited_promoter(self):
+        self.client.force_authenticate(user=self.promoter.person.user)
+
+        self.promoter_invited_state.delete()
+
+        response = self.client.post(self.url, data=self.accept_data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        json_response = response.json()
+
+        errors = json_response.get('non_field_errors', [])
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0]['status_code'], SignataireNonInviteException.status_code)
+
+    def test_accept_the_thesis_by_the_lead_promoter_with_no_adre_user(self):
+        self.client.force_authenticate(user=self.promoter.person.user)
+
+        self.jury_adre.delete()
+
+        response = self.client.post(self.url, data=self.accept_data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        json_response = response.json()
+
+        errors = json_response.get('non_field_errors', [])
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0]['status_code'], GestionnaireADRENonTrouveException.status_code)
+
+    def test_accept_the_thesis_by_the_lead_promoter(self):
+        self.client.force_authenticate(user=self.promoter.person.user)
+
+        response = self.client.post(self.url, data=self.accept_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.doctorate.refresh_from_db()
+        self.thesis_distribution_authorization.refresh_from_db()
+
+        # Check that the data have been updated
+        self.assertEqual(
+            self.thesis_distribution_authorization.status,
+            ChoixStatutAutorisationDiffusionThese.DIFFUSION_VALIDEE_PROMOTEUR.name,
+        )
+
+        # Check that the approval data have been saved
+        group = self.thesis_distribution_authorization.signature_group
+        self.assertIsNotNone(group)
+
+        promoters: QuerySet[Actor] = group.actors.filter(
+            thesisdistributionauthorizationactor__role=RoleActeur.PROMOTEUR.name,
+        )
+        self.assertEqual(len(promoters), 1)
+
+        self.assertEqual(promoters[0].person, self.promoter.person)
+        self.assertEqual(promoters[0].thesisdistributionauthorizationactor.rejection_reason, '')
+        self.assertEqual(
+            promoters[0].thesisdistributionauthorizationactor.internal_comment,
+            self.accept_data['commentaire_interne'],
+        )
+        self.assertEqual(promoters[0].thesisdistributionauthorizationactor.role, RoleActeur.PROMOTEUR.name)
+        self.assertEqual(promoters[0].comment, self.accept_data['commentaire_externe'])
+
+        promoter_states: QuerySet[StateHistory] = promoters[0].states.all()
+        self.assertEqual(len(promoter_states), 2)
+
+        self.assertEqual(promoter_states[1].state, SignatureState.APPROVED.name)
+
+        # Check that the adre manager has been invited
+        adre_managers: QuerySet[Actor] = group.actors.filter(
+            thesisdistributionauthorizationactor__role=RoleActeur.ADRE.name,
+        )
+        self.assertEqual(len(adre_managers), 1)
+
+        self.assertEqual(adre_managers[0].person, self.jury_adre.person)
+        self.assertEqual(adre_managers[0].comment, '')
+        self.assertEqual(adre_managers[0].thesisdistributionauthorizationactor.rejection_reason, '')
+        self.assertEqual(adre_managers[0].thesisdistributionauthorizationactor.internal_comment, '')
+        self.assertEqual(adre_managers[0].thesisdistributionauthorizationactor.role, RoleActeur.ADRE.name)
+
+        adre_states: QuerySet[StateHistory] = adre_managers[0].states.all()
+        self.assertEqual(len(adre_states), 1)
+
+        self.assertEqual(adre_states[0].state, SignatureState.INVITED.name)
+
+        # Check that the notification has been sent
+        self.assertEqual(EmailNotification.objects.count(), 1)
+
+        notification_to_student = EmailNotification.objects.filter(person=self.doctorate.student).first()
+        self.assertIsNotNone(notification_to_student)
+
+        email_message = message_from_string(notification_to_student.payload)
+        to_email_addresses = [address for _, address in getaddresses(email_message.get_all('To', [('', '')]))]
+        cc_email_addresses = [address for _, address in getaddresses(email_message.get_all('Cc', [('', '')]))]
+        self.assertEqual(len(to_email_addresses), 1)
+        self.assertEqual(to_email_addresses[0], self.jury_adre.person.email)
         self.assertEqual(len(cc_email_addresses), 1)
         self.assertEqual(cc_email_addresses, [''])
